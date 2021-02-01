9af6fe954a94ca94fe7a8c6a2df4a5b048656105
Cristian <crigerprogrammer@gmail.com>
2021-02-01 15:42:50 -0600

    ejercicios 7

--- ejercicios.sql
+++ ejercicios.sql
@@ -92,3 +92,16 @@ WHERE id IN (
     FROM platzi.alumnos
     WHERE tutor_id = 30
 )
+
+-- Extraer algo de un campo
+SELECT EXTRACT(YEAR FROM fecha_incorporacion AS anio_incorporacion)
+FROM platzi.alumnos;
+
+-- Otra forma
+SELECT DATE_PART('YEAR', fecha_incorporacion) AS anio_incorporacion
+FROM platzi.alumnos;
+
+SELECT DATE_PART('YEAR', fecha_incorporacion) AS anio_incorporacion,
+    DATE_PART('MONTH', fecha_incorporacion) AS mes_incorporacion,
+    DATE_PART('DAY', fecha_incorporacion) AS dia_incorporacion
+FROM platzi.alumnos;
