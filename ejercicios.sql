59e60a58cbeb081bf4458f591ddef8965ea9f874
Cristian <crigerprogrammer@gmail.com>
2021-02-01 14:36:04 -0600

    ejercicios 3

--- ejercicios.sql
+++ ejercicios.sql
@@ -10,3 +10,21 @@ FROM (
     FROM platzi.alumnos 
 ) AS alumnos_with_row_num
 WHERE row_id = 1;
+
+-- Sacar primeros 5
+SELECT *
+FROM platzi.alumnos
+FETCH FIRST 5 ROWS ONLY;
+
+-- Sacar primeros 5
+SELECT *
+FROM platzi.alumnos
+LIMIT 5;
+
+-- Sacar primeros 5 WFunction
+SELECT *
+FROM(
+    SELECT ROW_NUMBER() OVER() AS row_id, *
+    FROM platzi.alumnos
+) AS alumnos_with_row_num
+WHERE row_id BETWEEN 1 AND 5;
