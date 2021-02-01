7eec16aa4b78d33ca35f70e800d59c23ac663971
Cristian <crigerprogrammer@gmail.com>
2021-01-31 22:32:43 -0600

    where 2

--- Seleccion.sql
+++ Seleccion.sql
@@ -9,3 +9,13 @@ WHERE cantidad > 10;
 SELECT *
 FROM tabla_diaria
 WHERE cantidad < 100;
+
+SELECT *
+FROM tabla_diaria
+WHERE cantidad > 10
+AND cantidad < 100;
+
+SELECT *
+FROM tabla_diaria
+WHERE cantidad BETWEEN 10
+AND 100;
