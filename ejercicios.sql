74bea51ee9adb987b71e4f288946db2afce4fc5c
Cristian <crigerprogrammer@gmail.com>
2021-02-01 14:30:56 -0600

    ejercicios 2

--- ejercicios.sql
+++ ejercicios.sql
@@ -2,3 +2,11 @@
 SELECT *
 FROM platzi.alumnos
 LIMIT 1;
+
+-- Windows Function
+SELECT *
+FROM (
+	SELECT ROW_NUMBER() OVER() AS row_id, *
+    FROM platzi.alumnos 
+) AS alumnos_with_row_num
+WHERE row_id = 1;
