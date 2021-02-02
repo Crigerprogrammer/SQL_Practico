d03aabab1f78078e0aa46710b6149904d3cb02be
Cristian <crigerprogrammer@gmail.com>
2021-02-02 10:58:43 -0600

    reto 5

--- ejercicios.sql
+++ ejercicios.sql
@@ -212,3 +212,20 @@ SELECT *
 FROM platzi.alumnos
 WHERE tutor_id BETWEEN 1 AND 10;
 
+SELECT int4range(10, 20) @> 3;
+
+SELECT numrange(11.1, 22.2) && numrange(20.0, 30.0);
+
+-- El numero mayor del rango
+SELECT UPPER(int8range(15,25));
+
+-- El numero menor del rango
+SELECT LOWER(int8range(15,25));
+
+SELECT int4range(10,20) * int4range(15,25);
+
+SELECT ISEMPTY (numrange(1,5));
+
+SELECT *
+FROM platzi.alumnos
+WHERE int4range(10,20) @> tutor_id;
