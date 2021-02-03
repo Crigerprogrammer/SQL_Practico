51f389e62032b507d329406bae6a87a71273a88c
Cristian <crigerprogrammer@gmail.com>
2021-02-03 12:53:13 -0600

    practica 2

--- ejercicios.sql
+++ ejercicios.sql
@@ -304,4 +304,25 @@ LEFT JOIN platzi.carreras AS c
 ON a.carrera_id = c.id 
 WHERE c.id IS NULL 
 
+SELECT a.nombre,
+       a.apellido,
+       a.carrera_id,
+       c.id,
+       c.carrera
+FROM platzi.alumnos AS a 
+RIGHT JOIN platzi.carreras AS c 
+ON a.carrera_id = c.id 
+WHERE a.id IS NULL 
+
+SELECT a.nombre,
+       a.apellido,
+       a.carrera_id,
+       c.id,
+       c.carrera
+FROM platzi.alumnos AS a 
+INNER JOIN platzi.carreras AS c 
+ON a.carrera_id = c.id 
+ORDER BY c.id DESC;
+
+
 
