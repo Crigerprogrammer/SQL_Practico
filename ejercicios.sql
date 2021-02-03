08091246d9226cae9b6380a01573145aca4e0232
Cristian <crigerprogrammer@gmail.com>
2021-02-03 12:49:03 -0600

    practica

--- ejercicios.sql
+++ ejercicios.sql
@@ -292,3 +292,16 @@ FROM platzi.alumnos AS a
 LEFT JOIN platzi.carreras AS c 
 ON a.carrera_id = c.id 
 WHERE c.id IS NULL
+
+-- Todas las uniones
+SELECT a.nombre,
+       a.apellido,
+       a.carrera_id,
+       c.id,
+       c.carrera
+FROM platzi.alumnos AS a 
+LEFT JOIN platzi.carreras AS c 
+ON a.carrera_id = c.id 
+WHERE c.id IS NULL 
+
+
