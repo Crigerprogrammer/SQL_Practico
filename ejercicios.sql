b47d5c8643d7c33c3e56af2ed9b5dd4bffa5f76f
Cristian <crigerprogrammer@gmail.com>
2021-02-03 16:44:05 -0600

    practica 5

--- ejercicios.sql
+++ ejercicios.sql
@@ -344,3 +344,18 @@ FROM (
 ) AS alumnos_with_row_id 
 WHERE row_id <= 5
 ORDER BY carrera_id;
+
+-- Rangos
+SELECT * 
+FROM generate_series(1,4);
+
+SELECT *
+FROM generate_series(1.1,4,1.3);
+
+SELECT current_date + s.a. AS dates 
+FROM generate_series(0, 14, 7) AS s(a);
+
+SELECT *
+FROM generate_series('2020-09-01 00:00:00'::timestamp, 
+                     '2020-09-04 12:00:00', '10 hours');
+
